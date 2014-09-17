d5ce89386750dc2a08b0e162d057268b099f9858
prpavlov <pavelr.pavlov@telerik.com>
2014-09-17 10:22:32 +0300

    My first commit

--- _site_SL/radpivotgrid/Examples/radpivotgrid-features-aggregates/UserControl_Vb.xaml.vb
+++
@@ -1,5 +0,0 @@
-'#region "radpivotgrid-features-aggregates_1"
-Dim aggregate As New PropertyAggregateDescription()
-aggregate.PropertyName = "Price"
-aggregate.AggregateFunction = AggregateFunctions.StdDevP
-'#endregion

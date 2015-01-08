7be21afac96649b7180cf1366c8873bf9892ac6d
kalin <kalin.todorov@telerik.com>
2015-01-08 16:22:21 +0200

    Updated Deploy

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@
 
 VERSION="1.0.`date +%s`";
 
+
 PAYLOAD="{ \"Folder\": \"//telerik.com/resources/AppDirector/Drops/XAML/WPF.Zip\", \"Version\": \"$VERSION\", \"Environment\": \"Test\", \"RepositoryName\": \"Local\", \"Workspace\": \"Documentation\", \"ClientId\": \"uri:platform.cp.builds\", \"ClientSecret\": \"3edd5acdb61db37f3ab2bd735b058911\", \"SkipRegisterPackage\": false, \"OverwritePackage\": false, \"SkipDateChange\": true, \"SkipDeployment\": false, \"DeploymentApiUrl\": \"http://deploy.telerik.com/api/deployment/v1\", \"RepositoryUrl\": \"http://deploy.telerik.com/api/repository/v1\", \"TokenEndpoint\": \"https://identity.telerik.com/v2/oauth/telerik/token\", \"PackageNameFormat\": \"package-{0}-{1}.zip\" }";
 
 echo "posting $PAYLOAD";

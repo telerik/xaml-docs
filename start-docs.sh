658281fc1574d00844d66160bb1174894a05e1a1
Dilyan Traykov <dtraykov94@gmail.com>
2018-11-12 16:21:32 +0200

    Update start-docs.sh

--- start-docs.sh
+++ start-docs.sh
@@ -3,11 +3,12 @@
 config_file="_config.yml"	
 if [ ! -z "$1" ]	
   then 	
-    echo "Using configuration file: $1"	
-    config_file=$1	
+    config_file+=",$1"
 fi
 
-rm -rf _site/ && rm -rf .sass-cache && rm -rf .jekyll-cache && rm -rf .asset-cache
-#docker pull tmitev/docs-seed:site
-#docker build -t tmitev/docs-seed:site .
-docker run --rm --env CONFIG_FILE="_config.yml" --env JEKYLL_COMMAND="serve" --env JEKYLL_EXTRA="--watch --incremental --host 0.0.0.0" --name docs_site -t -v "/c:\work\git\xaml-docs:/app_root" -p 4000:4000 -t tmitev/docs-seed:site
+echo "Using configuration from: $config_file"	
+
+rm -rf _site/ .sass-cache .jekyll-cache .asset-cache
+#docker pull progressdocs/docs-seed:site
+#docker build -t progressdocs/docs-seed:site .
+winpty docker run --rm -it --env CONFIG_FILE=$config_file --env JEKYLL_COMMAND="serve" --env JEKYLL_EXTRA="--watch --incremental --host 0.0.0.0" --name docs_site -t -i -v /$(pwd):/app_root -p 4000:4000 -t progressdocs/docs-seed:site
